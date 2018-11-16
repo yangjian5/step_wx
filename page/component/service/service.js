3270312f5bddaffa32ae5a882e8870dea2a276a9
yangjian9 <yangjian9@staff.weibo.com>
2018-11-16 18:10:23 +0800

    1
    1

--- page/component/service/service.js
+++ page/component/service/service.js
@@ -4,5 +4,12 @@ Page({
   data:{
   },
   onLoad(options){
+  },
+  previewImage: function (e) {
+    var current = e.target.dataset.src;
+    wx.previewImage({
+      current: current, // 当前显示图片的http链接
+      urls: ['https://www.aiwsport.com/test/byql.png'] // 需要预览的图片http链接列表
+    })
   }
 })
